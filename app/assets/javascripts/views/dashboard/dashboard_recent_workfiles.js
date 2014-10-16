4f8f1094debf7ecf20a94ab1f9a3fc5e4f5ba798
Michel Milano <mmilano@alpinenow.com>
2014-10-15 22:17:12 -0700

    recent work files changes - altering the animation timings to be a little faster for the user - reduce default size of the select menus - refresh font (inherent) size is larger then previous, and size is based on font (inherent) size

--- app/assets/javascripts/views/dashboard/dashboard_recent_workfiles.js
+++ app/assets/javascripts/views/dashboard/dashboard_recent_workfiles.js
@@ -26,7 +26,7 @@ chorus.views.DashboardRecentWorkfiles = chorus.views.DashboardModule.extend({
         this.resource = this.collection = new chorus.collections.WorkfileSet(workfiles);
         this.render();
         if (this.$('#recent_workfiles_configuration').is(':visible')) {
-            this.$('#recent_workfiles_configuration').fadeOut(140);
+			this.$('#recent_workfiles_configuration').fadeOut(140);
         }
     },
 
@@ -48,7 +48,8 @@ chorus.views.DashboardRecentWorkfiles = chorus.views.DashboardModule.extend({
 
     showOptions: function(event) {
         event.preventDefault();
-        this.$('#recent_workfiles_configuration').fadeIn("fast");
+        this.$('#recent_workfiles_configuration').fadeIn(180);
+
         _.defer(_.bind(function () {
 //             this.$('#recent_workfiles_configuration .mask').css('height', this.$('#recent_workfiles_main_content').height() + 20 + 'px');
             chorus.styleSelect(this.$(".recent_items_select"));
